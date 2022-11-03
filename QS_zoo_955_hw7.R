6b09258a31a2efcc6bb945c528ad03ca3a3b3ac0
quinnlanconnorsmith <quinnlanconnorsmith@gmail.com>
2022-11-03 18:20:59 -0500

    1st hw commit

--- QS_zoo_955_hw7.R
+++ QS_zoo_955_hw7.R
@@ -3,6 +3,7 @@
 library(spatstat)
 library(maptools)
 
+
 #define variables
 #variables that control the size and strength of clusters
 val.at.center=1
@@ -70,6 +71,50 @@ points(centers,type="p", col="red", pch=21, bg="red")
 
 #create a point pattern object for analysis using the spatstat library
 output_ppp = ppp(output.X, output.Y, c(Xmin,Xmax), c(Ymin,Ymax))
-.
+plot(output_ppp)
+
+#create a CSR object for analysis 
+
+lambda.u <- function(x,y){1000 * x^2 * y^2 + 1000}
+output_csr <- rpoispp(lambda = lambda.u, win = owin(c(0,1), c(0,1)))
+plot(output_csr)
 
 
+####Q1.1####
+
+#The coordinates in the center matrix control the spatial extent of clusters, you can change where the centers
+#are located by manipulating the coordinates (e.g. changing everything to 50 will space it out)
+
+####Q1.2####
+#effect.range and background have major controls over the strength of clustering 
+#The Y and X min/max have some control over the strength of clustering 
+
+####Q1.3####
+#run these two plot functions 
+plot(output_ppp)
+plot(output_csr)
+
+####Q2####
+quadrat.test(output_ppp, alternative = "clustered")
+quadrat.test(output_csr, alternative = "regular")
+#PPP quadrat test X2 = 291.5, df = 24, p-value <2.2e-16
+#CSR quadrat test X2 = 46.633, df = 24, p-value 0.9963
+#P value close to 1 means this does not differ from CSR
+#(which hopefully makes sense, because it was coded as a true CSR)
+
+####Q3####
+
+#Sample example
+X <- runifpoint(100)
+K <- Kest(X)
+plot(K)
+
+pest <- Kest(output_ppp)
+plot(pest)
+cest <- Kest(output_csr)
+plot(cest)
+
+plot(envelope(output_ppp))
+plot(envelope(output_csr))
+#For the point pattern we generated, at small scales the points are randomly distributed, 
+#however as we increase scale it becomes obvious there is a pattern to the points - they're clustered 
